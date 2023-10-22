24d13b59b7615687f765207c526b112554d7ae0b
pranjalipongde09 <pranjalipongde9@gmail.com>
2023-10-22 18:19:13 +0530

    Shushiman Website added

---
+++ saas-website/js/main.js
@@ -0,0 +1,37 @@
+// FAQ accordion
+document.addEventListener("DOMContentLoaded", () => {
+  const faqContainer = document.querySelector(".faq-content");
+
+  faqContainer.addEventListener("click", (e) => {
+    const groupHeader = e.target.closest(".faq-group-header");
+
+    if (!groupHeader) return;
+
+    const group = groupHeader.parentElement;
+    const groupBody = group.querySelector(".faq-group-body");
+    const icon = groupHeader.querySelector("i");
+
+    //   toggle icon
+    icon.classList.toggle("fa-plus");
+    icon.classList.toggle("fa-minus");
+
+    //   toogle visibility of body
+
+    groupBody.classList.toggle("open");
+
+    //   close other open faq body
+    const otherGroups = faqContainer.querySelectorAll(".faq-group");
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body ");
+        const otherIcon = otherGroup.querySelector(".faq-group-header i ");
+
+        otherGroupBody.classList.remove("open");
+
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
+  });
+});
